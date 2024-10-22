2803633c8d6e847abf1c62d0faf2f0beccb08738
sachelaride42 <mateus.sachelaride@gmail.com>
2024-10-22 16:21:28 -0400

    Criação backend

---
+++ controller/pratoController.php
@@ -0,0 +1,35 @@
+<?php
+include_once 'modelos/classePrato.php';
+class pratoController
+{
+    public function abrirTelaCadastro() {
+        include 'views/prato/cadastro.php';
+    }
+
+    public function cadastrarPrato() {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $preco = filter_input(INPUT_POST, 'preco', FILTER_SANITIZE_NUMBER_FLOAT);
+            $descricao = filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            if($nome && $preco && $descricao){
+                $prato = new Prato();
+                $prato->setNome($nome);
+                $prato->setPreco($preco);
+                $prato->setDescricao($descricao);
+
+                if($prato->salvar()){ //para implementar depois na classe Prato
+                    $_SESSION['msg'] = "Prato cadastrado com sucesso!";
+                }else{
+                    $_SESSION['msg'] = "Erro ao cadastrar o prato!";
+                }
+            }
+            else{
+                $_SESSION['msg'] = "Dados inválidos";
+            }
+            header('Location: /view/prato/cadastro.php');
+            exit();
+        }
+    }
+
+}
---
+++ modelos/classePrato.php
@@ -0,0 +1,29 @@
+<?php
+class Prato{
+    private $id;
+    private $nome;
+    private $preco;
+    private $descricao;
+
+    public function getId(){
+        return $this->id;
+    }
+    public function getNome(){
+        return $this->nome;
+    }
+    public function getPreco(){
+        return $this->preco;
+    }
+    public function getDescricao(){
+        return $this->descricao;
+    }
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+    public function setPreco($preco){
+        $this->preco = $preco;
+    }
+    public function setDescricao($descricao){
+        $this->descricao = $descricao;
+    }
+}
